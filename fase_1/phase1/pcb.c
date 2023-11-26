d86d17f3d56024f8b29c3d76c4b6e04786a29de6
luca marangon <lucamarangon2001@gmail.com>
2023-11-26 17:39:24 +0100

    provato a insere le prime 3 funzioni, ma dà vari errori di tipi

--- fase_1/phase1/pcb.c
+++ fase_1/phase1/pcb.c
@@ -6,13 +6,31 @@ LIST_HEAD(pcbFree_h);
 static int next_pid = 1;
 
 void initPcbs() {
-	klog_print("here!");
+      klog_print("here!");
 }
 
 void freePcb(pcb_t *p) {
+  struct list_head *list_address = &(p->p_list);
+  list_add_tail(list_address,&pcbFree_h); //aggiunto in fondo
 }
 
 pcb_t *allocPcb() {
+  if(list_empty(&pcbFree_h) == 1) //se la lista è vuota
+    return NULL;
+  struct list_head *p = pcbFree_h.next;
+  list_del(p); //cancella p dalla sua lista
+  //reinizializzo il pcb che contiene il puntatore p
+  struct pcb_t *pcb_libero = container_of(p,pcb_t,p_list); //Macro che restituisce il puntatore all'istanza della struttura che contiene un certo list_head
+  /* dà errori di tipi, da guardare
+  LIST_HEAD_INIT(pcb_libero->p_list);
+  pcb_libero->p_parent = NULL;
+  LIST_HEAD_INIT(pcb_libero->p_child);
+  LIST_HEAD_INIT(pcb_libero->p_sib);
+  //pcb_libero->p_s = 0; //stato proc = ???
+  pcb_libero->p_time = 0;
+  LIST_HEAD_INIT(pcb_libero->msg_inbox);
+  pcb_libero->p_supportstruct = NULL;
+  */
 }
 
 void mkEmptyProcQ(struct list_head *head) {
