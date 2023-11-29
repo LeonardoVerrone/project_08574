7b62c4229e41310debc4027ca52ea9337d84cfe4
luca-mar-01 <lucamarangon2001@gmail.com>
2023-11-29 13:26:41 +0100

    aggiunte le prime 3 funzioni di msg.c similmente a come fatte per pcb.c

--- fase_1/phase1/msg.c
+++ fase_1/phase1/msg.c
@@ -4,12 +4,37 @@ static msg_t msgTable[MAXMESSAGES];
 LIST_HEAD(msgFree_h);
 
 void initMsgs() {
+  INIT_LIST_HEAD(&msgFree_h);
+
+  for (int i = 0; i < MAXMESSAGES; i++) {
+    // aggiungo a msgFree m_list di ogni msg
+    list_add(&msgTable[i].m_list, &msgFree_h);
+  }
 }
 
 void freeMsg(msg_t *m) {
+  if(m == NULL)
+    return;
+  
+  // devo rimuoverlo dalla coda dei msg del pcb a cui appartiene(pcb.msg_inbox)
+  list_del(&m->m_list);
+  // e aggiungerlo ai msg liberi
+  list_add(&m->m_list, &msgFree_h);
 }
 
 msg_t *allocMsg() {
+  if( list_empty(&msgFree_h) )
+    return NULL;
+  
+  msg_t *m = container_of(msgFree_h.next, msg_t, m_list);
+  list_del(msgFree_h.next);
+  
+  //inizializzo
+  INIT_LIST_HEAD(&m->m_list);
+  m->m_sender = NULL;
+  m->m_payload = 0;
+  
+  return m;
 }
 
 void mkEmptyMessageQ(struct list_head *head) {
